985be602246751275d3ff9fecfb2db12e346682d
Unknown <wariffon@gmail.com>
2018-11-28 19:50:44 +0700

    front+backv1 by Fon

--- static/newStaffjs.js
+++ static/newStaffjs.js
@@ -6,66 +6,113 @@ new Vue({
         lastName:'',
         email:'',
         tel:'',
-        status:'',
+        status:'Staff',
         username:'',
         password:'',
         cfPassword:''
-      }
+      },
+        rules: {
+          alphaStr: [
+            { required: true, message: 'Please Input Information', trigger: 'change' },
+            { min: 3, max: 30, message: 'Length should be 3 to 30', trigger: 'change' },
+            { pattern: '^[A-Za-z]+$', message: 'Input Must be a Letters', trigger: 'change' }
+          ],
+          telNo: [
+            { required: true, message: 'Please Input Telephone Number', trigger: 'change' },
+            { min: 9, max: 10, message: 'Length should be 9 to 10', trigger: 'change' },
+            { pattern: '^[0-9]+$', message: 'Input must be a Numbers', trigger: 'change' }
+          ],
+          emailAddr: [
+            { required: true, message: 'Please Input Email', trigger: 'change' },
+            { type:'email', message: 'Invalid Email', trigger: 'change' }
+          ],
+          username: [
+            { required: true, message: 'Please Input Username', trigger: 'change' },
+            { min: 3, max: 15, message: 'Length should be 3 to 15', trigger: 'change' },
+            { pattern: '^[A-Za-z0-9]+$', message: 'Input Must be Letters or Numbers', trigger: 'change' }
+          ],
+          password: [
+            { required: true, message: 'Please Input Password', trigger: 'change' },
+            { min: 6, pattern: '(?=.*[0-9])(?=.*[A-Za-z])', required: true,
+            message: 'Password must be a conbination of at least 6 numbers letters', trigger: 'change' }
+            // { min: 8, message: 'Length should more than 8 Characters', trigger: 'change' },
+            // { pattern: '(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])', required: true,
+            // message: 'Password must be at least one number, uppercase and lowercase letter', trigger: 'change' }
+          ],
+          rePassword: [
+            { required: true, message: 'Please Input Password Again', trigger: 'change' },
+            { min: 6, pattern: '(?=.*[0-9])(?=.*[A-Za-z])', required: true,
+            message: 'Password must be a conbination of at least 6 numbers letters', trigger: 'change' }
+            // { min: 8, message: 'Length should more than 8 Characters', trigger: 'change' },
+            // { pattern: '(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])', required: true,
+            // message: 'Password must be at least one number, uppercase and lowercase letter', trigger: 'change' }
+          ]
+        }
     },
     methods: {
       createAccount() {
-          axios({
-               method: 'post',
-               url: 'created/',
-               data: {
-                    'firstname':this.form.username,
-                    'lastname':this.form.lastName,
-                    'tel':this.form.tel,
-                    'email':this.form.email,
-                    'status':this.form.status,
-                    'username':this.form.username,
-                    'password':this.form.password,
-               },
-               xsrfCookieName: 'csrftoken',
-               xsrfHeaderName: 'X-CSRFToken',
-               headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'Content-Type': 'application/json //x-www-form-urlencoded; charset=UTF-8'},
-               }).then(res => {
-                       window.location.href="../";
+          this.$refs.form.validate((valid) => {
+            if (valid) {
+              if(this.form.password === this.form.cfPassword){
+                this.$message({
+                  message: 'Account Created',
+                  type: 'success',
+                  center: true
+                });
+                axios({
+                   method: 'post',
+                   url: 'created/',
+                   data: {
+                        'firstname':this.form.username,
+                        'lastname':this.form.lastName,
+                        'tel':this.form.tel,
+                        'email':this.form.email,
+                        'status':this.form.status,
+                        'username':this.form.username,
+                        'password':this.form.password,
+                   },
+                   xsrfCookieName: 'csrftoken',
+                   xsrfHeaderName: 'X-CSRFToken',
+                   headers: {
+                            'X-Requested-With': 'XMLHttpRequest',
+                            'Content-Type': 'application/json //x-www-form-urlencoded; charset=UTF-8'},
+                   }).then(res => {
+//                           window.location.href="../";
                });
-
-
-        console.log(this.form.firstName,this.form.lastName,this.form.email,this.form.tel,this.form.status);
-        this.form.firstName = '';
-        this.form.lastName = '';
-        this.form.email = '';
-        this.form.tel = '';
-        this.form.status = '';
-        this.username = '',
-        this.password = '',
-        this.cfPassword = ''
-        this.$message({
-          message: 'Account Created',
-          type: 'success',
-          center: true
-        });
+                this.$refs.form.resetFields();
+              }
+              else {
+                this.$message({
+                  message: 'Password not Match',
+                  type: 'error',
+                  center: true
+                });
+              }
+            }
+            else {
+              this.$message({
+                message: 'Invalid Information',
+                type: 'error',
+                center: true
+              });
+            }
+          });
       },
       discard() {
-        axios({
-             method: 'get',
-             url: '../staff',
-             }).then(res => {
-                     window.location.href="../staff";
-             });
+//        axios({
+//             method: 'get',
+//             url: '../staff',
+//             }).then(res => {
+//                     window.location.href="../staff";
+//             });
         this.form.firstName = '';
         this.form.lastName = '';
         this.form.email = '';
         this.form.tel = '';
-        this.form.status = '';
-        this.username = '',
-        this.password = '',
-        this.cfPassword = ''
+        this.form.status = 'Staff';
+        this.form.username = '';
+        this.form.password = '';
+        this.form.cfPassword = '';
         this.$message({
           message: 'Change Discarded',
           type: 'error',
--- static/staffjs.js
+++ static/staffjs.js
@@ -1,4 +1,12 @@
 new Vue({
+  el:"#staff",
+  data:{
+    firstName:'',
+    lastName:'',
+  }
+});
+
+new Vue({
   el:"#searching",
   data() {
     return {
@@ -71,50 +79,105 @@ new Vue({
       firstName:'',
       lastName:'',
       email:'',
-      tel:''
+      tel:'',
+      username:'',
+      password:'',
+      cfPassword:''
     },
-    dialogAccount:false
+    dialogAccount:false,
+    rules: {
+      alphaStr: [
+        { required: true, message: 'Please Input Information', trigger: 'change' },
+        { min: 3, max: 30, message: 'Length should be 3 to 30', trigger: 'change' },
+        { pattern: '^[A-Za-z]+$', message: 'Input Must be a Letters', trigger: 'change' }
+      ],
+      telNo: [
+        { required: true, message: 'Please Input Telephone Number', trigger: 'change' },
+        { min: 9, max: 10, message: 'Length should be 9 to 10', trigger: 'change' },
+        { pattern: '^[0-9]+$', message: 'Input must be a Numbers', trigger: 'change' }
+      ],
+      emailAddr: [
+        { required: true, message: 'Please Input Email', trigger: 'change' },
+        { type:'email', message: 'Invalid Email', trigger: 'change' }
+      ],
+      username: [
+        { required: true, message: 'Please Input Username', trigger: 'change' },
+        { min: 3, max: 15, message: 'Length should be 3 to 15', trigger: 'change' },
+        { pattern: '^[A-Za-z0-9]+$', message: 'Input Must be Letters or Numbers', trigger: 'change' }
+      ],
+      password: [
+        { required: true, message: 'Please Input Password', trigger: 'change' },
+        // { min: 8, message: 'Length should more than 8 Characters', trigger: 'change' },
+        { min: 6, pattern: '(?=.*[0-9])(?=.*[A-Za-z])', required: true,
+        message: 'Password must be a conbination of at least 6 numbers letters', trigger: 'change' }
+      ],
+      rePassword: [
+        { required: true, message: 'Please Input Password Again', trigger: 'change' },
+        // { min: 8, message: 'Length should more than 8 Characters', trigger: 'change' },
+        { min: 6, pattern: '(?=.*[0-9])(?=.*[A-Za-z])', required: true,
+        message: 'Password must be a conbination of at least 6 numbers letters', trigger: 'change' }
+        // { pattern: '(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])', required: true,
+        // message: 'Password must be at least one number, uppercase and lowercase letter', trigger: 'change' }
+      ]
+    }
   },
   methods: {
     createAccount() {
-     
-      axios({
-        method: 'post',
-        url: '../oak/',
-        data: {
-             'firstname':this.form.firstName,
-             'lastname':this.form.lastName,
-             'email':this.form.email,
-             'tel':this.form.tel,
-             'username':this.form.username,
-             'password':this.form.password,
-
-        },
-        xsrfCookieName: 'csrftoken',
-        xsrfHeaderName: 'X-CSRFToken',
-        headers: {
-                 'X-Requested-With': 'XMLHttpRequest',
-                 'Content-Type': 'application/json //x-www-form-urlencoded; charset=UTF-8'},
-        }).then(res => {
-          console.log('aas');
-                window.location.href="../";
-        });
-
-
-   //console.log(this.form.firstName,this.form.lastName,this.form.email,this.form.tel);
-   //this.$refs[formName].resetFields();
-   this.dialogAccount = false;
-   this.$message({
-     message: 'Account Created',
-     type: 'success',
-     center: true
-   });
+        this.$refs.form.validate((valid) => {
+        if (valid) {
+          if(this.form.password === this.form.cfPassword){
+            this.$message({
+              message: 'Account Created',
+              type: 'success',
+              center: true
+            });
+            axios({
+                method: 'post',
+                url: '../oak/',
+                data: {
+                     'firstname':this.form.firstName,
+                     'lastname':this.form.lastName,
+                     'email':this.form.email,
+                     'tel':this.form.tel,
+                     'username':this.form.username,
+                     'password':this.form.password,
+                },
+                xsrfCookieName: 'csrftoken',
+                xsrfHeaderName: 'X-CSRFToken',
+                headers: {
+                         'X-Requested-With': 'XMLHttpRequest',
+                         'Content-Type': 'application/json //x-www-form-urlencoded; charset=UTF-8'},
+                }).then(res => {
+                  console.log('aas');
+                });
+            this.dialogAccount = false;
+            this.$refs.form.resetFields();
+          }
+          else {
+            this.$message({
+              message: 'Password not Match',
+              type: 'error',
+              center: true
+            });
+          }
+        }
+        else {
+          this.$message({
+            message: 'Invalid Information',
+            type: 'error',
+            center: true
+          });
+        }
+      });
     },
     discard() {
       this.form.firstName = '';
       this.form.lastName = '';
       this.form.email = '';
       this.form.tel = '';
+      this.form.username = '';
+      this.form.password = '';
+      this.form.cfPassword = '';
       this.dialogAccount = false;
       this.$message({
         message: 'Change Discarded',
